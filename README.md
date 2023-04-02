d60989fd73a1bfe96b0ee58c98ae702e2f2b44d4
Samuel <samfrank41802@gmail.com>
2023-04-01 22:02:55 -0400

    landing page still in progress but first of many commits

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DandyHive
 This is the web application for DandyHive, a platform for students to look for mentors and mentees.
+
+## Instructions to run
+To create virtual environment: `python -m venv "DandyHive"`
+To go to virtual environment: `bin source/activate`
+
+To start: `python3 ./src/App.py`
--- src/App.py
+++ src/App.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "/static")
 
 @app.route("/")
 def home():
-    return "<h1>Hello World</h1>"
+    return render_template('landing_page.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
     app.run()
